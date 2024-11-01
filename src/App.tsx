6be919c66d255cbc7f0a42ceed2ae51d345c1d14
Igor Cheburek <igorcheburek@outlook.com>
2024-11-01 13:37:37 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyBtn from './MyBtn';
+import "./App.css"
+import { useState } from 'react';
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+    function btnClickHandler(){
+        alert(count);
+        setCount(count + 1)
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    { products.map(x => <MyBtn key={x.id} count={count} btnClickHandler={btnClickHandler}/>)}
+  </>
   );
 }
 
